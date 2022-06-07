1c5e7bf21da937a8f78464563767d0975f081d75
Cesar <mekoxplay@gmail.com>
2022-06-06 21:56:57 -0500

    mekox

--- comandos/🔰info/ping.js
+++ comandos/🔰info/ping.js
@@ -10,6 +10,7 @@ module.exports = {
         .setThumbnail("https://images-ext-1.discordapp.net/external/qZwfTW5TZ0wih24zg9LLf2eSa5klfnGyYYbM9ZSrPEM/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/957720942639972383/a98dbc322d6da17e3a2a7f8a3a813330.webp?width=683&height=683")
         .addField("Ping", `${mekox.ws.ping}` + "ms <:ping:901864024529076234> ")
         .addField("Latencia", `${Date.now() - message.createdTimestamp}` + "ms <:ping:901864024529076234> ")
+        .setFooter(`${message.author.tag}`)
         .setTimestamp()
         .setColor("#ccb494")
         message.reply({ embeds: [embed] })
